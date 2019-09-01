7be7cbe9b6ea9ea3e515bb560ee01cc32f27577c
ri0t <riot@c-base.org>
2019-09-01 22:22:02 +0200

    Fixed license and version_info string

--- formal/version.py
+++ formal/version.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# Isomer - The distributed application framework
-# ==============================================
-# Copyright (C) 2011-2019 Heiko 'riot' Weinen <riot@c-base.org> and others.
+# Formal
+# ======
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Copyright 2013 Rob Britton
+# Copyright 2015-2019 Heiko 'riot' Weinen <riot@c-base.org> and others.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# This file has been changed and this notice has been added in
+# accordance to the Apache License
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-__author__ = "Heiko 'riot' Weinen"
-__license__ = "AGPLv3"
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 """Version Module
 
-Unified Isomer wide version number.
+Unified Formal version number.
 """
 
 try:
     from formal.scm_version import version
 
-    version_info = version
+    version_info = version.split(".")
 except ImportError as e:
     version_info = (0, 6, 4, "dev")  # (major, minor, patch, dev?)
     version = ".".join(map(str, version_info)) if version_info[-1] != "dev" else "dev"
